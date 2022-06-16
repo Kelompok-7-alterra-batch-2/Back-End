659567827ea6e0d228d4250793794be282120c19
tien26 <martienirfan@gmail.com>
2022-06-16 20:25:38 +0700

    added swagger documentation api

---
+++ src/main/java/com/hospital/hospitalmanagement/config/ConfigSwagger.java
@@ -0,0 +1,38 @@
+package com.hospital.hospitalmanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
+@Configuration
+@EnableSwagger2
+public class ConfigSwagger {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hospital.hospitalmanagement.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+    private ApiInfo metaData(){
+        ApiInfo apiInfo = new ApiInfo(
+                "REST API Hospital Management",
+                "Spring Boot REST API Hospital Management Kelompok  7",
+                "1.0",
+                "Terms of service",
+                "Kelompok 7",
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0");
+        return apiInfo;
+
+    }
+}
