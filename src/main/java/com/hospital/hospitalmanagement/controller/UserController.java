6773d66a8a42aead520c1281be00cde6e2a400d5
Michael Gerrard <hutagalung.michael23@gmail.com>
2022-07-08 11:37:57 +0700

    edit app.properties and add annotation CrossOrigin in userController

--- src/main/java/com/hospital/hospitalmanagement/controller/UserController.java
+++ src/main/java/com/hospital/hospitalmanagement/controller/UserController.java
@@ -4,13 +4,11 @@ import com.hospital.hospitalmanagement.controller.dto.EmailPasswordDTO;
 import com.hospital.hospitalmanagement.controller.response.GetTokenDTO;
 import com.hospital.hospitalmanagement.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
