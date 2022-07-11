2088adec5e09defc9986de907a7c3ce452ec1f3a
Michael Gerrard <hutagalung.michael23@gmail.com>
2022-07-11 23:31:21 +0700

    add endpoints for getDoctorByEmail

--- src/main/java/com/hospital/hospitalmanagement/service/UserServiceImpl.java
+++ src/main/java/com/hospital/hospitalmanagement/service/UserServiceImpl.java
@@ -138,8 +138,6 @@ public class UserServiceImpl implements UserDetailsService {
                 .name(doctor.getName())
                 .id(doctor.getId())
                 .email(doctor.getEmail())
-                .availableTo(doctor.getAvailableTo())
-                .availableFrom(doctor.getAvailableFrom())
                 .dob(doctor.getDob())
                 .role(doctor.getRole())
                 .nid(doctor.getNid())
