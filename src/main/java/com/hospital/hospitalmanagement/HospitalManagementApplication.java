1649a91fce28412ff2f061ffcbf3d3cf0dad49aa
MichaelGerrardStanleyH <hutagalung.michael23@gmail.com>
2022-07-13 19:33:08 +0700

    add configuration in main class

--- src/main/java/com/hospital/hospitalmanagement/HospitalManagementApplication.java
+++ src/main/java/com/hospital/hospitalmanagement/HospitalManagementApplication.java
@@ -2,9 +2,28 @@ package com.hospital.hospitalmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HospitalManagementApplication {
+	@Configuration
+	public class CorsConfig implements WebMvcConfigurer {
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedOrigins("*")
+							.allowedHeaders("*");
+				}
+			};
+		}
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalManagementApplication.class, args);
